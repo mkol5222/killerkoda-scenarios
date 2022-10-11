11a71e54834593aeb14deeea0dbc7871e565bc6e
Martin Koldovsky <mkoldov@gmail.com>
2022-10-11 11:04:19 +0200

    fg

--- appsec-linux/foreground.sh
+++ appsec-linux/foreground.sh
@@ -1,9 +1,6 @@
 echo "Installing scenario..."
 
-echo "Packages"
-apt install jq -y
-
-echo "Services"
+echo "Starting services"
 docker run -d -p 8080:3000 public.ecr.aws/f4q1i2m2/acmeaudit
 docker ps
 
