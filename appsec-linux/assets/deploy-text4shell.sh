ec774b81386d756538bd4f0cae4705468b1e4da7
Martin Koldovsky <mkoldov@gmail.com>
2022-10-20 14:05:10 +0200

    text4shell

--- appsec-linux/assets/deploy-text4shell.sh
+++ appsec-linux/assets/deploy-text4shell.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+echo 'Stopping Acme Audit'
 docker stop acmeaudit
 
+echo 'Getting text4shell'
 cd
 git clone https://github.com/karthikuj/cve-2022-42889-text4shell-docker.git
 cd cve-2022-42889-text4shell-docker/
 
+echo 'Installing Java and Maven'
 sudo apt update
-sudo apt install maven
+sudo apt install maven -y
 mvn clean install
+
+echo 'Building docker container'
 docker build --tag=text4shell .
-docker run -p 8080:8080 text4shell
+
+echo 'text4shell server starting'
+docker run -p 8080:8080 -d --name text4shell text4shell
+docker ps
+echo 'Done'
