f3d4b6888587c1578568c3323a55329d8494d5bc
Martin Koldovsky <mkoldov@gmail.com>
2022-11-16 23:11:10 +0100

    v106

--- zz_appsec-training-embedded-profile/step4-verify.sh
+++ zz_appsec-training-embedded-profile/step4-verify.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 CODE=$(curl -s -o /dev/null -w "%{http_code}" "$APP_URL?q=UNION+1=1")
-[[ "$CODE" = "403" ]]
+if [[ "$CODE" = "403" ]] ; then
+    touch /tmp/appsec-Enforcing
+fi
+stat /tmp/appsec-Enforcing
