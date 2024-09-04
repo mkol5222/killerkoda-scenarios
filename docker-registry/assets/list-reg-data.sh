b01e24c39c473e647c1f78ea1bf9dc8c64db1706
Martin Koldovsky <mkoldov@checkpoint.com>
2024-09-04 13:34:37 +0200

    re reg

--- docker-registry/assets/list-reg-data.sh
+++ docker-registry/assets/list-reg-data.sh
@@ -2,3 +2,6 @@
 
 docker volume ls | grep registry 
 docker run --rm -it -v registry_data:/data ubuntu find /data/
+
+curl -u read:vpn123 -k https://reg.localtest.me/v2/_catalog
+curl -u write:vpn123 -k https://reg.localtest.me/v2/_catalog
--- docker-registry/assets/makepasswd.sh
+++ docker-registry/assets/makepasswd.sh
@@ -3,4 +3,16 @@
 # echo -n 'devops:' > htpasswd
 # openssl passwd -apr1 >> htpasswd
 
-docker run --rm --entrypoint htpasswd httpd -Bbn devops vpn123 >htpasswd
+# read only
+echo -n > htpasswd-ro
+docker run --rm --entrypoint htpasswd httpd -Bbn read vpn123 >>htpasswd-ro
+# write and read
+echo -n > htpasswd-rw
+docker run --rm --entrypoint htpasswd httpd -Bbn write vpn123 >>htpasswd-rw
+docker run --rm --entrypoint htpasswd httpd -Bbn write vpn123 >>htpasswd-ro
+
+cat htpasswd-ro
+cat htpasswd-rw
+
+curl -u write:vpn123 -k https://reg.localtest.me/v2/_catalog
+curl -u read:vpn123 -k https://reg.localtest.me/v2/_catalog
