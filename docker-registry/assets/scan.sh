6e262d6e01751753d2c51d24c1910d434f05f653
Martin Koldovsky <mkoldov@checkpoint.com>
2024-09-13 11:43:18 +0200

    scan logic

--- docker-registry/assets/scan.sh
+++ docker-registry/assets/scan.sh
@@ -1,46 +1,126 @@
 #!/bin/bash
 
-. ./.env-shiftleft
-echo $CHKP_CLOUDGUARD_ID
+# cd ./docker-registry/assets/
 
-curl --unix-socket /var/run/docker.sock -X GET "http://localhost/images/node:22/get" > /tmp/image.tar
-curl --unix-socket /var/run/docker.sock -X GET "http://localhost/images/hello-world:latest/get" > /tmp/image.tar
-file /tmp/image.tar
-tar tvf /tmp/image.tar
-
-docker run --rm -v ./res:/res -v /tmp:/data -e CHKP_CLOUDGUARD_ID=$CHKP_CLOUDGUARD_ID -e CHKP_CLOUDGUARD_SECRET=$CHKP_CLOUDGUARD_SECRET -e SHIFTLEFT_REGION=$SHIFTLEFT_REGION -e SHIFTLEFT_ENV=$SHIFTLEFT_ENV checkpoint/shiftleft:latest_v2 shiftleft -D image-scan -j -i /data/image.tar -e $SHIFTLEFT_ENV -o /res/scan.json
+# set creds
+# . ./env-shiftleft
+# env | grep SHIFTLEFT_
+# env | grep CLOUDGUARD_
 
 
-IMAGEID=$(curl -s --unix-socket /var/run/docker.sock -X GET "http://localhost/images/node:22/json" | jq -r '.Id')
-IMAGEID=$(echo $IMAGEID | cut -c 8-)
-echo $IMAGEID 
-cd docker-registry/assets/
-. ./env-shiftleft
-env | grep SHIFTLEFT_
-env | grep CLOUDGUARD_
+# if [[ -z $CHKP_CLOUDGUARD_ID ]]; then
+#     echo "Error: Checkpoint CloudGuard ID is not set"
+#     # exit 1
+# fi
 
-LINK="https://portal.checkpoint.com/dashboard/cloudguard#/workload/images/generic?cloudAccountId=$SHIFTLEFT_ENV&assetType=ShiftLeftImage&assetId=sha256%3A$IMAGEID&tabName=overview&tabOnly=true&platform=shiftleft&drawer=undefined"
-echo $LINK
+# export image - eg. node:22 or hello-world:latest
+function export_image() {
+    IMAGE=$1
+    echo "Exporting image $IMAGE"
+    curl --unix-socket /var/run/docker.sock -X GET "http://localhost/images/$IMAGE/get" > /tmp/image.tar
+    file /tmp/image.tar
+    tar tvf /tmp/image.tar
+}
 
-AUTH="$CHKP_CLOUDGUARD_ID:$CHKP_CLOUDGUARD_SECRET"
-echo $AUTH
+# export_image node:22
+# export_image debian:latest
+# export_image hello-world:latest
 
-IMAGEID=$(curl -s --unix-socket /var/run/docker.sock -X GET "http://localhost/images/node:22/json" | jq -r '.Id')
-APIHOST="api.dome9.com"
-APIURL="https://${APIHOST}/v2/vulnerability/scan-metadata?EnvironmentId=${SHIFTLEFT_ENV}&EntityType=ShiftLeftImage&EntityId=${IMAGEID}"
-echo $APIURL
+function scanResults() {
+    # cat ./res/scan.json
+    # cat ./res/scan.json | jq '. | keys'
+    # cat ./res/scan.json | jq '.assessments[0] | keys'
+    # cat ./res/scan.json | jq '.assessments[0].testEntities | keys'
+    # cat ./res/scan.json | jq '.assessments[0].testEntities.scanSummary'
+    # cat ./res/scan.json | jq '.assessments[0].testEntities.imageScan' | less
+    cat ./res/scan.json | jq '.assessments[0].testEntities.imageScan[0].totals' 
+}
 
-curl -vvv -H "Content-Type: application/json" -u $AUTH $APIURL | jq .
+# scanResults
 
-echo curl -vvv -H "Content-Type: application/json" -u $AUTH $APIURL 
+function scanMetadata() {
+    IMAGE=$1
+    IMAGEID=$(curl -s --unix-socket /var/run/docker.sock -X GET "http://localhost/images/$IMAGE/json" | jq -r '.Id')
 
-cat ./res/scan.json
-cat ./res/scan.json | jq '. | keys'
-cat ./res/scan.json | jq '.assessments[0] | keys'
-cat ./res/scan.json | jq '.assessments[0].testEntities | keys'
-cat ./res/scan.json | jq '.assessments[0].testEntities.scanSummary'
-cat ./res/scan.json | jq '.assessments[0].testEntities.imageScan' | less
-cat ./res/scan.json | jq '.assessments[0].testEntities.imageScan[0].totals' 
+    AUTH="$CHKP_CLOUDGUARD_ID:$CHKP_CLOUDGUARD_SECRET"
+    
 
-# ~/w/scan1.json
-# link to Infinity Portal:
+    APIHOST="api.dome9.com"
+    APIURL="https://${APIHOST}/v2/vulnerability/scan-metadata?EnvironmentId=${SHIFTLEFT_ENV}&EntityType=ShiftLeftImage&EntityId=${IMAGEID}"
+    
+
+    curl -s -H "Content-Type: application/json" -u $AUTH $APIURL | jq .
+}
+
+scanMetadata ubuntu:latest
+
+# /v2/kubernetes/imageAssurance/image/general?id=13|7f1fedb8-0584-4473-91c9-ff3b9047fe81|Image|sha256:edbfe74c41f8a3501ce542e137cf28ea04dd03e6df8c9d66519b6ad761c2598a
+
+function scanResultsInDb() {
+    IMAGE=$1
+    IMAGEID=$(curl -s --unix-socket /var/run/docker.sock -X GET "http://localhost/images/$IMAGE/json" | jq -r '.Id')
+
+    AUTH="$CHKP_CLOUDGUARD_ID:$CHKP_CLOUDGUARD_SECRET"
+    
+    ID="13|$SHIFTLEFT_ENV|Image|$IMAGEID"
+    URLENCODED_ID=$(echo -n $ID | jq -sRr @uri)
+
+    APIHOST="api.dome9.com"
+    APIURL="https://${APIHOST}/v2/kubernetes/imageAssurance/image/general?id=${URLENCODED_ID}"
+    
+
+    curl -s -H "Content-Type: application/json" -u $AUTH $APIURL | jq .
+}
+
+# scanResultsInDb ubuntu:latest
+
+
+
+
+function scan_image() {
+    IMAGE=$1
+    docker pull $IMAGE
+    export_image $IMAGE
+    echo "Scanning image $IMAGE"
+    docker run --rm -v ./res:/res -v /tmp:/data -e CHKP_CLOUDGUARD_ID=$CHKP_CLOUDGUARD_ID -e CHKP_CLOUDGUARD_SECRET=$CHKP_CLOUDGUARD_SECRET -e SHIFTLEFT_REGION=$SHIFTLEFT_REGION -e SHIFTLEFT_ENV=$SHIFTLEFT_ENV checkpoint/shiftleft:latest_v2 shiftleft -D image-scan -j -i /data/image.tar -e $SHIFTLEFT_ENV -o /res/scan.json
+
+    IMAGEID=$(curl -s --unix-socket /var/run/docker.sock -X GET "http://localhost/images/$IMAGE/json" | jq -r '.Id')
+    if [[ $IMAGEID == sha256:* ]]; then
+        echo "IMAGEID is valid"
+    else
+        echo "IMAGEID is invalid"
+    fi
+
+    scanResults
+
+    SHORT_IMAGEID=$(echo $IMAGEID | cut -c 8-)
+    LINK="https://portal.checkpoint.com/dashboard/cloudguard#/workload/images/generic?cloudAccountId=$SHIFTLEFT_ENV&assetType=ShiftLeftImage&assetId=sha256%3A$SHORT_IMAGEID&tabName=overview&tabOnly=true&platform=shiftleft&drawer=undefined"
+    echo 
+    echo "Infinity Portal link to scan results: $LINK"
+    echo
+}
+
+scan_image ubuntu:latest
+scan_image checkpoint/shiftleft:latest_v2
+scan_image hello-world:latest
+
+scanResultsInDb hello-world:latest | jq .image.scanDetails.totals
+scanResultsInDb ubuntu:latest | jq .image.scanDetails.totals
+
+function scanIfNeeded() {
+    IMAGE=$1
+    RES=$(scanResultsInDb $IMAGE | jq -r .image.scanDetails.totals)
+    if [[ $RES != "null" ]]; then
+        echo "Scan results found"
+    else
+        echo "Scan results not found"
+        scan_image $IMAGE
+    fi
+}
+
+scanIfNeeded debian:latest
+
+
+
+
+
